7fc06c7e454c4df1d747e33e8012fb23ce5614e3
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-11-12 00:34:03 +0530

    [Umesh]Added: finding prime factor of a number and storing in a array

--- Array/Arrays.js
+++ Array/Arrays.js
@@ -10,3 +10,18 @@ arr.sort();
 
 console.log("Second largest element is: "+arr[arr.length-2]);
 console.log("Second smallest element is: "+arr[1]);
+
+// UC3
+let n = 36;
+let array = new Array();
+
+for(let i=2; i<=Math.sqrt(n);i++){
+    if(n%i==0){
+        array.push(i);
+        n /= i;
+    }
+}
+if(array.length==0){
+    array.push(n);
+}
+console.log(array);
